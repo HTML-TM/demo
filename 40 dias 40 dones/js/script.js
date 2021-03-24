1aacd8d484386912f9fc245244d9d725c838284f
tairo <socialtairo@gmail.com>
2021-03-24 18:29:58 +0100

    leccion js

---
+++ 40 dias 40 dones/js/script.js
@@ -0,0 +1,6 @@
+function ab(){
+    document.getElementById("ab").innerHTML = "y se transformó gracias a un archivo externo"
+}
+function ac(){
+    document.getElementById("ac").innerHTML = "y se transformó gracias a un archivo externo"
+}
