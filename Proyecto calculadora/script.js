47d0b21a16979a1045af338122104d80cb94b3d2
tairo <socialtairo@gmail.com>
2021-04-20 22:53:11 +0100

    adding Git commit lesson

---
+++ Proyecto calculadora/script.js
@@ -0,0 +1,15 @@
+const sumar = (num1,num2) => {
+    return parseInt(num1) + parseInt(num2)
+}
+const restar = (num1,num2) => {
+    return parseInt(num1) + parseInt(num2)
+}
+const multiplicar = (num1,num2) => {
+    return parseInt(num1) + parseInt(num2)
+}
+const dividir = (num1,num2) => {
+    return parseInt(num1) + parseInt(num2)
+}
+
+alert("¿que operacion deseas hacer?");
+operacion = prompt("1: suma, 2: resta. 3: divide, 4: multiplicación")
